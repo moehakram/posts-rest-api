6b3d4440b7fe351708a654ffce2b1b8e55d3f50c
moehakram <moeh.akram@gmail.com>
2025-07-30 04:41:46 +0800

    Refactor code post-repository

--- src/repositories/post-repository.js
+++ src/repositories/post-repository.js
@@ -10,60 +10,32 @@ export function findAll() {
 }
 
 export function findById(id) {
-    // const post = posts.find(post => post.id === Number(id));
-    // return Promise.resolve(post);
-
-    return new Promise((resolve, reject) => {
-        const post = posts.find(post => post.id === Number(id));
-        resolve(post);
-    });
-
-
+    const post = posts.find(post => post.id === Number(id));
+    return Promise.resolve(post);
 }
 
 export function findBySlug(slug) {
-
-    return new Promise((resolve, reject) => {
-        const post = posts.find(post => post.slug === slug);
-        resolve(post);
-    });
-
+    const post = posts.find(post => post.slug === slug);
+    return Promise.resolve(post);
 }
 
 export async function save(post) {
-    return new Promise(async (resolve, reject) => {
-        posts.push(post);
-        await writeFile(file, JSON.stringify(posts, null, 2));
-        resolve(post);
-    });
+    posts.push(post);
+    await writeFile(file, JSON.stringify(posts, null, 2));
+    return post;
 }
-export function update(post) {
-    return new Promise(async (resolve, reject) => {
-        const index = posts.findIndex(p => post.id === p.id);
-        posts[index] = { ...post };
-        await writeFile(file, JSON.stringify(posts, null, 2));
-        resolve(post);
-    })
+export async function update(post) {
+    const index = posts.findIndex(p => post.id === p.id);
+    posts[index] = { ...post };
+    await writeFile(file, JSON.stringify(posts, null, 2));
+    return post;
 }
 
-export function remove(id) {
-    // return new Promise(async (resolve, reject) => {
-    //     const index = posts.findIndex(p => Number(id) === p.id);
-    //     if (index !== -1) {
-    //         const result = posts.splice(index, 1);
-    //         await writeFile(file, JSON.stringify(posts, null, 2));
-    //         console.log({ index, result });
-    //         resolve(result);
-    //     } else {
-    //         resolve();
-    //     }
-    // });
+export async function remove(id) {
+    const index = posts.findIndex(p => Number(id) === p.id);
+    if (index === -1) return null;
 
-    return new Promise(async (resolve, reject) => {
-        const result = posts.filter(post => post.id !== Number(id));
-        if (result.length !== 0) {
-            await writeFile(file, JSON.stringify(result, null, 2));
-        }
-        resolve();
-    });
+    const result = posts.splice(index, 1);
+    await writeFile(file, JSON.stringify(posts, null, 2));
+    return result;
 }
